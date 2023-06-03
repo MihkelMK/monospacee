760bca404c15b536f3a9b5b422de6a1e4df6e9a3
Mihkel Martin Kasterpalu <43957228+MihkelMK@users.noreply.github.com>
2023-06-03 18:32:10 +0300

    refactor for some clarity

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,6 @@
 import { dev } from '$app/environment';
 
 export const title = 'monospacee';
-export const description = 'GPLv3 litsensiga DJ duo';
+export const description = 'DJ duo with a GPLv3 license. Coming soon to a terminal near you!';
+export const site_name = 'monospacee';
 export const url = dev ? 'http://localhost:5173/' : 'https://monospac.ee';
--- src/routes/blog/category/[tag]/+page.ts
+++ src/routes/blog/category/[tag]/+page.ts
@@ -5,6 +5,6 @@ export const load = (async ({ fetch, params }) => {
 	const response = await fetch('/api/posts');
 	const posts: Post[] = await response.json();
 
-	posts.filter((post) => params.slug in post.categories);
-	return { posts: posts, category: params.slug };
+	posts.filter((post) => params.tag in post.categories);
+	return { posts: posts, category: params.tag };
 }) satisfies PageLoad;
