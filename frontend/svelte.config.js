6026d15fc2cf96a7c37f40cbd3d31530d449bd59
Mihkel Martin Kasterpalu <43957228+MihkelMK@users.noreply.github.com>
2023-08-28 00:43:05 +0300

    small markdown fixes fix typos don't wrap lines of text in markdown open links from markdown in new tabs

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -40,21 +40,25 @@ const mdsvexOptions = {
 	rehypePlugins: [
 		rehypeSlug,
 		[rehypeShiftHeading, { shift: 1 }],
-		[rehypeToc, { customizeTOC: addTracklist }],
 		[
 			rehypeRewrite,
 			{
 				rewrite: (node) => {
-					if (
-						node.type === 'element' &&
-						node.tagName === 'section' &&
-						node.children.some((child) => child.type === 'raw' && child.value.includes('Gallery'))
-					) {
-						node.properties.className = 'hasGallery';
+					if (node.type === 'element') {
+						if (
+							node.tagName === 'section' &&
+							node.children.some((child) => child.type === 'raw' && child.value.includes('Gallery'))
+						) {
+							node.properties.className = 'hasGallery';
+						} else if (node.tagName === 'a') {
+							node.properties.target = '_blank';
+							node.properties.rel = ['nofollow', 'noopener'];
+						}
 					}
 				}
 			}
 		],
+    		[rehypeToc, { customizeTOC: addTracklist }],
 		rehypePresetMinify
 	],
 	remarkPlugins: [remarkSqueezeParagraphs, remarkCapitalizeHeadings, sectionize]
