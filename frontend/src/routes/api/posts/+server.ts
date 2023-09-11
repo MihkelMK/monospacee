db0a22949f46333567350f7eeeec3d7dddebaa75
Mihkel Martin Kasterpalu <43957228+MihkelMK@users.noreply.github.com>
2023-09-12 00:02:57 +0300

    social/attribution refactor make social links a seperate component for better use in posts add title and source link values to gallery images better handle posts without recordings

--- frontend/src/routes/api/posts/+server.ts
+++ frontend/src/routes/api/posts/+server.ts
@@ -1,49 +1,49 @@
-import { json } from "@sveltejs/kit";
-import type { Post } from "$lib/types";
+import { json } from '@sveltejs/kit';
+import type { Post } from '$lib/types';
 
 function getPosts() {
-  let posts: Post[] = [];
-  const paths = import.meta.glob("/src/posts/*.md", { eager: true });
+	let posts: Post[] = [];
+	const paths = import.meta.glob('/src/posts/*.md', { eager: true });
 
-  for (const path in paths) {
-    const file = paths[path];
+	for (const path in paths) {
+		const file = paths[path];
 
-    if (file && typeof file === "object" && "metadata" in file) {
-      const metadata = file.metadata as Omit<Post, "date">;
-      const date = path
-        .split("/")
-        .at(-1)
-        ?.replace(".md", "")
-        .toLowerCase()
-        .replace(" ", "-")
-        .replace("_", "-");
+		if (file && typeof file === 'object' && 'metadata' in file) {
+			const metadata = file.metadata as Omit<Post, 'date'>;
+			const date = path
+				.split('/')
+				.at(-1)
+				?.replace('.md', '')
+				.toLowerCase()
+				.replace(' ', '-')
+				.replace('_', '-');
 
-      const post = { ...metadata, date } satisfies Post;
-      post.published && posts.push(post);
-    }
-  }
+			const post = { ...metadata, date } satisfies Post;
+			posts.push(post);
+		}
+	}
 
-  posts = posts.sort(
-    (first, second) =>
-      new Date(second.date).getTime() - new Date(first.date).getTime(),
-  );
+	posts = posts.sort(
+		(first, second) => new Date(second.date).getTime() - new Date(first.date).getTime()
+	);
 
-  return posts;
+	return posts;
 }
 
 export function GET({ url, setHeaders }) {
-  setHeaders({
-    "cache-control": "max-age=60",
-  });
+	setHeaders({
+		'cache-control': 'max-age=60'
+	});
 
-  const start = Number(url.searchParams.get("start")) ?? 0;
-  const end = Number(url.searchParams.get("end")) ?? 0;
+	const start = Number(url.searchParams.get('start')) ?? 0;
+	const end = Number(url.searchParams.get('end')) ?? 0;
 
-  const allPosts = getPosts();
-  const posts = allPosts.slice(start, end > 0 ? end : undefined);
+	const allPosts = getPosts();
+	const posts = allPosts.slice(start, end > 0 ? end : undefined);
 
-  const nextIdx = start + posts.length;
-  const nextFrom = nextIdx < allPosts.length ? nextIdx : null;
+	const nextIdx = start + posts.length;
+	const nextFrom = nextIdx < allPosts.length ? nextIdx : null;
+	console.log(start, end, allPosts);
 
-  return json({ posts, nextFrom });
+	return json({ posts, nextFrom });
 }
