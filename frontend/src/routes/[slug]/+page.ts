def6efe9a4950f289b45082e82bc388190f7c05b
Mihkel Martin Kasterpalu <43957228+MihkelMK@users.noreply.github.com>
2024-07-01 04:16:32 +0300

    Update glob usage

--- frontend/src/routes/[slug]/+page.ts
+++ frontend/src/routes/[slug]/+page.ts
@@ -2,19 +2,19 @@ import { error } from '@sveltejs/kit';
 import type { Cue } from '$lib/types.js';
 
 export async function load({ params, fetch, setHeaders }) {
-		setHeaders({
-			'cache-control': 'max-age=60'
-		});
+	setHeaders({
+		'cache-control': 'max-age=60'
+	});
 
 	try {
 		const post = await import(`../../posts/${params.slug}.md`);
 
-    if (!post.metadata.recording) {
-      return {
-        content: post.default,
-        meta: post.metadata
-      }
-    }
+		if (!post.metadata.recording) {
+			return {
+				content: post.default,
+				meta: post.metadata
+			};
+		}
 
 		const cueResponse = await fetch(`/api/cues/${params.slug}.cue`);
 		const cue: Cue = cueResponse.json();
@@ -22,7 +22,7 @@ export async function load({ params, fetch, setHeaders }) {
 		return {
 			content: post.default,
 			meta: post.metadata,
-			cue: cue
+			cue: await cue
 		};
 	} catch (_err) {
 		throw error(404, `Could not find ${params.slug}`);
