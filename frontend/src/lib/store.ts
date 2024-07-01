acf85ff9e0157a335e9df01232683b1867634565
Mihkel Martin Kasterpalu <43957228+MihkelMK@users.noreply.github.com>
2024-07-01 23:54:04 +0300

    Fix 500 error on page reload
    The default selectedRecording value previously set was not an existing
    post. This caused the page to error out.

--- frontend/src/lib/store.ts
+++ frontend/src/lib/store.ts
@@ -8,7 +8,7 @@ export const feed: Writable<Post[]> = writable([]);
 export const recordingPlaying: Writable<boolean> = writable(false);
 
 export const selectedRecording: Writable<string> = writable(
-	fromLocalStorage('selectedRecording', '2023-08-24') as string
+	fromLocalStorage('selectedRecording', '2024-08-24') as string
 );
 toLocalStorage(selectedRecording, 'selectedRecording');
 
@@ -18,7 +18,7 @@ export const cueJump: Writable<number | undefined> = writable(undefined);
 
 export const streamingData: Writable<Record<string, RecordingData>> = writable(
 	fromLocalStorage('streamingData', {
-		[fromLocalStorage('selectedRecording', '2024-06-08') as string]: {
+		[fromLocalStorage('selectedRecording', '2024-08-24') as string]: {
 			progress: 0
 		}
 	}) as Record<string, RecordingData>
