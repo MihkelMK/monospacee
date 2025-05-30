ac95112c3729d508c57f4ef5e1b63dee198436a3
Mihkel Martin Kasterpalu <43957228+MihkelMK@users.noreply.github.com>
2025-05-30 12:47:04 +0300

    Rewrite OG generation API. Doesn't need to be separate anymore

--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -7,7 +7,7 @@ export const description = 'DJ duo with a GPLv3 license. Coming soon to a termin
 
 export const site_name = 'monospacee';
 
-export const ogUrl = dev ? 'http://localhost:5174' : 'https://og.monospac.ee';
+export const ogUrl = dev ? 'http://localhost:5173/api/og' : 'https://monospac.ee/api/og';
 
 export const url = dev ? 'http://localhost:5173' : 'https://monospac.ee';
 
--- frontend/src/routes/api/og/[[size]]/OGBig.svelte
+++ frontend/src/routes/api/og/[[size]]/OGBig.svelte
@@ -1,24 +1,25 @@
 <script>
+	import Kern from '$lib/components/Kern.svelte';
+
 	/** @type {string} */
 	export let postType;
-
 	const accentColor =
 		postType === 'main'
 			? '#a160cd'
 			: postType === 'event'
-			? '#95d9f4'
-			: postType === 'stream'
-			? '#a160cd'
-			: '#88898c';
+				? '#95d9f4'
+				: postType === 'stream'
+					? '#a160cd'
+					: '#88898c';
 
 	const accentColorLow =
 		postType === 'main'
 			? '#95d9f4'
 			: postType === 'event'
-			? '#bfe8f8'
-			: postType === 'stream'
-			? '#b480d7'
-			: '#e7eaee';
+				? '#bfe8f8'
+				: postType === 'stream'
+					? '#b480d7'
+					: '#e7eaee';
 </script>
 
 <div
@@ -32,56 +33,45 @@
 		flex-direction: column;
 		flex-wrap: nowrap;
     background-image: linear-gradient(30deg, #14171c, #1c2027);
-	"
->
+	">
 	<div
 		style="
-      font-size: 4rem;
+      font-size: 18rem;
       display: flex;
       color: #BDC3CB;
       letter-spacing: -0.025em;
-      top: 17.5rem;
-      position: absolute;
-		"
-	>
-		<span style="color: {accentColor}; font-weight: 700; margin-top: -0.15em;">monospacee</span>
-		<span style="color: #BDC3CB; font-size: 3.5rem; margin-left: 0.25rem;">$</span>
+      line-height: 1.75em;
+      margin-top: 35rem;
+		">
+		<span style="color: {accentColor}; margin-top: -3rem;">monospacee</span>
+		<span style="font-size:16rem; margin-left: 2rem">$</span>
 	</div>
 	<div
 		style="
-      font-size: 5rem;
+      font-size: 26rem;
+      font-weight: 700;
       background-image: linear-gradient(30deg, {accentColor}, {accentColorLow});
       background-clip: text;
       -webkit-background-clip: text;
       color: transparent;
       letter-spacing: -0.05em;
-      padding: 0 5rem;
-      line-height: 1.08em;
-      margin-top: 11rem;
-		"
-	>
+      line-height: 1em;
+      margin-top: -8rem;
+		">
 		~/{postType === 'main' ? '' : `${postType.toLowerCase()}s`}
 	</div>
-	<span
+
+	<div
 		style="
     position: absolute;
-    top: 8rem;
-    left: 43%;
-    gap: 0.05em;
-    font-size: 4.5rem;
-
-      font-weight: 700;
+    top: 50%;
+    left: 50%;
+    display: flex;
+    transform: translate(-50%, -125%);
+    font-size: 18rem;
+    font-weight: 700;
 		color: {accentColorLow};
-"
-	>
-		<span style="width: 0.5em;">[</span><!--
---><span
-			style="width: 0.6em; font-size: 0.9em; margin-left: -0.2em;">＇</span
-		><!--
---><span style="">-</span><!--
---><span
-			style="width: 0.65em; font-size: 0.9em; margin-left: -0.25em;">＇</span
-		><!--
---><span style="width: 0.5em;">]</span>
-	</span>
+">
+		<Kern og></Kern>
+	</div>
 </div>
