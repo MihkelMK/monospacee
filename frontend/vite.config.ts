9a47b7b234cbcba79d922a67e15e234ad0fab8bc
Mihkel Martin Kasterpalu <43957228+MihkelMK@users.noreply.github.com>
2025-06-03 16:08:46 +0300

    Init paraglide

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,18 @@
+import { paraglideVitePlugin } from '@inlang/paraglide-js';
 import { defineConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { imagetools } from '@zerodevx/svelte-img/vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), imagetools()],
+	plugins: [
+		sveltekit(),
+		imagetools(),
+		paraglideVitePlugin({
+			project: './project.inlang',
+			outdir: './src/lib/paraglide'
+		})
+	],
 	build: {
-		rollupOptions: {
-			external: ['@resvg/resvg-js']
-		}
+		rollupOptions: { external: ['@resvg/resvg-js'] }
 	}
 });
