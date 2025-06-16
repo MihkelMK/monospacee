2a7a28337aad81fc1e409332cb1eddea19b50dc3
Mihkel Martin Kasterpalu <43957228+MihkelMK@users.noreply.github.com>
2025-06-16 13:07:00 +0300

    Automatically set newest post as fallback selected recording in Docker build

--- frontend/src/routes/+layout.ts
+++ frontend/src/routes/+layout.ts
@@ -1,26 +1,34 @@
+import { PUBLIC_LATEST_POST } from '$env/static/public';
 import { deLocalizeUrl } from '$lib/paraglide/runtime.js';
+import type { LoadEvent } from '@sveltejs/kit';
+
+const isValidRecording = async (fetch: LoadEvent['fetch'], slug: string) => {
+	const recordingPath = '/recordings/' + slug + '.mp3';
+	const recordingExists = await fetch(recordingPath, { method: 'HEAD' }).then((res) => {
+		return res.status === 200 || res.status === 204;
+	});
+
+	return recordingExists ? recordingPath : undefined;
+};
 
 export async function load({ url, fetch }) {
+	let recording: string | undefined;
+
 	const slug = deLocalizeUrl(url).pathname.split('/').at(-1);
 	const cueSlugOnLoad = url.searchParams.has('load');
 
 	// If loading post with ?load
 	// try setting post as selected recording (if that post has a recording file)
 	if (slug && cueSlugOnLoad) {
-		const recordingPath = '/recordings/' + slug + '.mp3';
-		const recordingExists = await fetch(recordingPath, { method: 'HEAD' }).then((res) => {
-			return res.status === 200 || res.status === 204;
-		});
+		recording = await isValidRecording(fetch, slug);
+	}
 
-		if (recordingExists) {
-			return {
-				url: url.pathname,
-				selected: recordingPath
-			};
-		}
+	if (!recording) {
+		recording = await isValidRecording(fetch, PUBLIC_LATEST_POST);
 	}
 
 	return {
-		url: url.pathname
+		url: url.pathname,
+		selected: recording
 	};
 }
